9cecf4f6ea4250184bc0719e77c5924ea3a79888
pongdang <pongdang0121@gmail.com>
2021-09-28 23:31:06 +0900

    blog.pongdang.me 시작!

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+function App() {
+  return (
+    <Container>
+      <Button>버튼1</Button>
+      <Button danger>버튼2</Button>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: pink;
+`;
+
+const Button = styled.button`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 120px;
+  color: white;
+  font-weight: 600;
+  -webkit-appearance: none;
+  cursor: pointer;
+  &:active,
+  &:focus {
+    outline: none;
+  }
+  background-color: ${(props) => (props.danger ? "#e74c3c" : "#2ecc71")};
+`;
+
+export default App;
