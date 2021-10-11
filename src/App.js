f825a799f2edb78420c69e2c4c61901d6588d9f1
pongdang <pongdang0121@gmail.com>
2021-10-11 21:45:09 +0900

    재활용할 수 있는 스타일

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import styled, { createGlobalStyle, css, keyframes } from "styled-components";
+import styled, { createGlobalStyle, css } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 body {
@@ -6,60 +6,39 @@ body {
   padding:0;
 }
 `;
+
+const awesomeCard = css`
+  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+  background-color: white;
+  padding: 20px;
+  border-radius: 50px;
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  ${awesomeCard};
+  background-color: pink;
+`;
+
+const Input = styled.input.attrs({
+  // 속성을 넣어줄 수 있다
+  required: true,
+  placeholder: "hello",
+})`
+  border: none;
+  ${awesomeCard};
+`;
+
 function App() {
   return (
     <>
       <GlobalStyle />
       <Container>
-        <Button>버튼1</Button>
-        <Button danger rotationTime={3}>
-          버튼2
-        </Button>
-        <Anchor href="http://google.com">Go to Google</Anchor>
+        <Input />
       </Container>
     </>
   );
 }
-
-const Container = styled.div`
-  height: 100vh;
-  width: 100%;
-  background-color: pink;
-`;
-
-const Button = styled.button`
-  border-radius: 50px;
-  padding: 5px;
-  min-width: 120px;
-  color: white;
-  font-weight: 600;
-  -webkit-appearance: none;
-  cursor: pointer;
-  &:active,
-  &:focus {
-    outline: none;
-  }
-  background-color: ${(props) => (props.danger ? "#e74c3c" : "#2ecc71")};
-  ${(props) => {
-    if (props.danger) {
-      return css`
-        animation: ${rotation} ${props.rotationTime}s linear infinite;
-      `;
-    }
-  }}
-`;
-
-const Anchor = styled(Button.withComponent("a"))`
-  text-decoration: none;
-`;
-
-const rotation = keyframes`
-from {
-  transform: rotate(0deg);
-}
-to {
-  transform: rotate(360deg);
-}
-`;
 
 export default App;
