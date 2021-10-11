e571ce19add213a75c7b75d5f17cde366349f88e
pongdang <pongdang0121@gmail.com>
2021-10-11 21:21:44 +0900

    돌아가는 버튼 (애니메이션)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import styled, { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle, css, keyframes } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 body {
@@ -12,7 +12,9 @@ function App() {
       <GlobalStyle />
       <Container>
         <Button>버튼1</Button>
-        <Button danger>버튼2</Button>
+        <Button danger rotationTime={3}>
+          버튼2
+        </Button>
         <Anchor href="http://google.com">Go to Google</Anchor>
       </Container>
     </>
@@ -38,10 +40,26 @@ const Button = styled.button`
     outline: none;
   }
   background-color: ${(props) => (props.danger ? "#e74c3c" : "#2ecc71")};
+  ${(props) => {
+    if (props.danger) {
+      return css`
+        animation: ${rotation} ${props.rotationTime}s linear infinite;
+      `;
+    }
+  }}
 `;
 
 const Anchor = styled(Button.withComponent("a"))`
   text-decoration: none;
+`;
+
+const rotation = keyframes`
+from {
+  transform: rotate(0deg);
+}
+to {
+  transform: rotate(360deg);
+}
 `;
 
 export default App;
